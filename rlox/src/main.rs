9668fff7ffd68f7c66760fb7f11d609bbd0c4058
Ye Yangchen <eikasia30@gmail.com>
2023-04-19 17:25:40 -0500

    Use rustyline for REPL

--- rlox/src/main.rs
+++ rlox/src/main.rs
@@ -1,11 +1,10 @@
-use std::{
-  io::{stdin, stdout, Write},
-  path::PathBuf,
-};
+use std::path::PathBuf;
 
 use clap::Parser;
 use miette::Result;
 use rlox::Interpreter;
+use rustyline::error::ReadlineError;
+use rustyline::DefaultEditor;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
@@ -29,29 +28,28 @@ fn run_file(file: PathBuf, interpreter: &mut Interpreter) -> Result<()> {
 }
 
 fn run_interactive(interpreter: &mut Interpreter) -> Result<()> {
-  for source_line in prompt_stream() {
-    let source = source_line.strip_suffix("\n").unwrap();
-    if let Err(report) = interpreter.run(source) {
-      println!("{}", report);
+  let mut rl = DefaultEditor::new().unwrap();
+
+  loop {
+    let readline = rl.readline("> ");
+    match readline {
+      Ok(line) => {
+        if let Err(report) = interpreter.run(&line) {
+          println!("{}", report);
+        }
+        rl.add_history_entry(line).unwrap();
+      }
+      Err(ReadlineError::Interrupted) => {
+        println!("CTRL-C");
+      }
+      Err(ReadlineError::Eof) => {
+        break;
+      }
+      Err(err) => {
+        println!("Error: {:?}", err);
+        break;
+      }
     }
   }
   Ok(())
-}
-
-fn prompt_stream() -> impl Iterator<Item = String> {
-  const PROMPT: &str = "> ";
-  /// Handles outputting a prompt and getting back a user-typed source string
-  struct PromptStream;
-  impl Iterator for PromptStream {
-    type Item = String;
-    fn next(&mut self) -> Option<Self::Item> {
-      let mut out = stdout();
-      write!(&mut out, "{}", PROMPT).unwrap();
-      out.flush().unwrap();
-      let mut s = String::new();
-      stdin().read_line(&mut s).ok()?;
-      Some(s)
-    }
-  }
-  PromptStream
 }
